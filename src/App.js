ef6c8584402a50ebb8e4b158849030bb498bce77
aminovitch-web <aminovitchweb@gmail.com>
2024-08-29 11:15:00 +0200

    implement lazy loading for routes to improve initial load peformance

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Suspense, lazy } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ClipLoader from "react-spinners/ClipLoader";
 import "./App.css";
 import Header from "./Component/Header";
-import Index from "./Page/Index";
-import Error from "./Component/Error";
-import AddEmployee from "./Page/AddEmployee";
 import { useUserInteractionStore } from "./Zustand/store";
+
+
+const Index = lazy(() => import("./Page/Index"));
+const Error = lazy(() => import("./Component/Error"));
+const AddEmployee = lazy(() => import("./Page/AddEmployee"));
 
 function App() {
   const pageTitle = useUserInteractionStore((state) => state.pageTitle);
+
   useEffect(() => {
     document.title = pageTitle;
   }, [pageTitle]);
@@ -16,11 +20,13 @@ function App() {
   return (
     <Router>
       <Header />
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="*" element={<Error />} />
-        <Route path="/addEmployee" element={<AddEmployee />} />
-      </Routes>
+      <Suspense fallback={<ClipLoader color="#36d7b7" size={50} />}>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="*" element={<Error />} />
+          <Route path="/addEmployee" element={<AddEmployee />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
